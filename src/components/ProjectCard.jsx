9846944190652ed6e7bde302fc7ccd5a3684444f
josimarrenepont <jnrenepont@hotmail.com>
2024-10-16 18:12:11 -0300

    ProjectCard and Projects class created

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const ProjectCard = ({ project }) => {
+  return (
+    <div className="flex bg-white p-4 rounded shadow-lg w-full">
+      {/* Imagem / GIF do projeto */}
+      <div className="w-2/3 p-4 flex justify-center">
+        <img 
+          src={project.image} 
+          alt={project.title} 
+          style={{ width: '60%', height: '400px' }}
+        />
+      </div>
+
+      {/* Descrição do projeto com dangerouslySetInnerHTML */}
+      <div className="w-1/3 p-4 flex flex-col justify-center">
+        <h3 className="text-2xl font-bold mb-4">{project.title}</h3>
+        <p className="text-gray-600 mb-4" dangerouslySetInnerHTML={{ __html: project.description }}></p>
+        <a href={project.link} className="hover:underline" style={{ color: 'white' }}>
+          Código Fonte
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import clinic from '../assets/clinic.gif';
+import errosGif from '../assets/erros.gif';
+import ProjectCard from './ProjectCard';
+
+const Projects = () => {
+  const projects = [
+    {
+      title: 'Separation Errors',
+      description: 'Back-end: Java | Spring Boot | Front-end: React | TypeScript | Vite | PostgreSQL.<br />' +
+                   'Projeto para controlar erros na separação de produtos na logística.',
+      image: errosGif,
+      link: 'https://github.com/josimarrenepont/separation-errors',
+    },
+    {
+      title: 'Physiotherapy Clinic',
+      description: 'Back-end: Java | Spring Boot | Front-end: React | TypeScript | Vite | PostgreSQL | Docker.<br />' +
+                   'O plano foi elaborado visando registrar clientes e seus dependentes em uma clínica de fisioterapia, bem como gerenciar o controle interno dos pacientes.',
+      image: clinic,
+      link: 'https://github.com/josimarrenepont/physiotherapy-s-clinic',
+    },
+  ];
+
+  return (
+    <section id="projects" className="bg-gray-200 py-12">
+      <div className="container mx-auto">
+        <h2 className="text-4xl font-bold text-center mb-8">Projetos</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project, index) => (
+            <ProjectCard key={index} project={project} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
