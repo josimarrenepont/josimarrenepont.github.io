26ca6367bcb647d76c54b9edf0fc3620bffc643e
josimarrenepont <jnrenepont@hotmail.com>
2024-10-16 19:12:41 -0300

    updated project gh-pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom'; // Importando HashRouter
 import App from './App';
 import './index.css';
 
-
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router> {/* Usando HashRouter para funcionar corretamente no GitHub Pages */}
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
