87edd69b836269328dc6bb9e69f71199e90dbedc
josimarrenepont <jnrenepont@hotmail.com>
2024-10-16 18:45:32 -0300

    Added gif model projects

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Projects from './components/Projects';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <About />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
