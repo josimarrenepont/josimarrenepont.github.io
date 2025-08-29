3fd14a5b54a46bb38f5afa71b5d23b8700e8fff9
josimarrenepont <jnrenepont@hotmail.com>
2025-08-28 23:24:45 -0300

    Updated the css styles of the classes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,36 +12,41 @@ const Header = () => {
   
   return (
     <header className="header">
-      <div className="header-content">
-        <div className="header-title">
-          <h1>Josimar Renepont</h1>
-          <p>Back-End Developer</p>
-        </div>
-        <div className="tag-icons">
-          <img src={javaIcon} alt="Java" className="tag-icon" />
-          <img src={kotlinIcon} alt="Kotlin" className="tag-icon" />
-          <img src={springIcon} alt="Spring" className="tag-icon" />
-          <img src={postgreSQLIcon} alt="PostgreSQL" className="tag-icon" />
-          <img src={dockerIcon} alt="Docker" className="tag-icon" />
-          <img src={kubernetesIcon} alt="Kubernetes" className="tag-icon" />
-        </div>
-        <nav>
-          <ul>
-            <li><a href="#about">Sobre</a></li>
-            <li><a href="#projects">Projetos</a></li>
-            <li><a href="#contact">Contato</a></li>
-          </ul>
-        </nav>
-        <div className="icons">
-          <a href="https://github.com/josimarrenepont">
-            <img src={githubIcon} alt="GitHub" />
-          </a>
-          <a href="https://www.linkedin.com/in/josimar-renepont/">
-            <img src={linkedinIcon} alt="LinkedIn" />
-          </a>
-        </div>
-      </div>
-    </header>
+  <div className="header-content">
+    <div className="header-title">
+      <h1>Josimar Renepont</h1>
+      <p>Back-End Developer</p>
+    </div>
+    <div className="tag-icons">
+      <img src={javaIcon} alt="Java" className="tag-icon" />
+      <img src={kotlinIcon} alt="Kotlin" className="tag-icon" />
+      <img src={springIcon} alt="Spring" className="tag-icon" />
+      <img src={postgreSQLIcon} alt="PostgreSQL" className="tag-icon" />
+      <img src={dockerIcon} alt="Docker" className="tag-icon" />
+      <img src={kubernetesIcon} alt="Kubernetes" className="tag-icon" />
+    </div>
+  </div>
+
+  {/* SEGUNDA LINHA - Menu + Links sociais */}
+  <div className="header-bottom">
+    <nav>
+      <ul>
+        <li><a href="#about">Sobre</a></li>
+        <li><a href="#projects">Projetos</a></li>
+        <li><a href="#contact">Contato</a></li>
+      </ul>
+    </nav>
+    <div className="icons">
+      <a href="https://github.com/josimarrenepont">
+        <img src={githubIcon} alt="GitHub" />
+      </a>
+      <a href="https://www.linkedin.com/in/josimar-renepont/">
+        <img src={linkedinIcon} alt="LinkedIn" />
+      </a>
+    </div>
+  </div>
+</header>
+
   );
 };
 
