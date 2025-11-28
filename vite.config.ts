c94609281056c21c62c04398044eb1e11bd91e7f
josimarrenepont <jnrenepont@hotmail.com>
2025-11-27 23:49:57 -0300

    build: atualização do portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/my-portfolio/", // 🔥 nome exato do repositório no GitHub
+});
